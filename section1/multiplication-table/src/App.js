3fd62105d2a8a5499420c37266e724aaa773e000
callmejeje <choije401@naver.com>
2022-08-04 22:01:42 +0900

    feat: 질문 밀리는 것 해결 완료

--- section1/multiplication-table/src/App.js
+++ section1/multiplication-table/src/App.js
@@ -1,14 +1,5 @@
-// import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-
-// const Answer = ({ onChange }) => {
-//   return (
-//     <div className="answer">
-//       <input type="number" onChange={onChange}></input>
-//     </div>
-//   );
-// };
 
 function App() {
   const [answer, setAnswer] = useState(undefined);
@@ -19,52 +10,26 @@ function App() {
     Math.ceil(Math.random() * 9),
   ]);
 
-  // const Question = () => {
-  //   return <div>{`${nums[0]} 곱하기 ${nums[1]}는?`}</div>;
-  // };
-
-  // function isCorrect(num1, num2, answer) {
-  //   return Number(num1) * Number(num2) === Number(answer);
-  // }
-
   const onChangeAnswer = (e) => {
     setAnswer(e.target.value);
   };
 
   const onClickCorrect = () => {
-    // setCorrect(isCorrect(nums[0], nums[1], answer));
     if (Number(nums[0]) * Number(nums[1]) === Number(answer)) {
       setCorrect(true);
       setNums([Math.ceil(Math.random() * 9), Math.ceil(Math.random() * 9)]);
     } else {
       setCorrect(false);
     }
-    // onClickSetNum();
   };
-
-  // const onClickSetNum = () => {
-  //   console.log("correct: " + correct);
-  //   if (correct === true) {
-  //     setNums([Math.ceil(Math.random() * 9), Math.ceil(Math.random() * 9)]);
-  //   } else {
-  //     let num0 = nums[0];
-  //     let num1 = nums[1];
-  //     setNums([num0, num1]);
-  //   }
-  // };
 
   return (
     <div className="App">
       <div>{`${nums[0]} 곱하기 ${nums[1]}는?`}</div>
-      {/* <Question /> */}
       <input type="number" onChange={onChangeAnswer}></input>
-      {/* <Answer onChange={onChangeAnswer} /> */}
-      {/* <button onClick={() => isCorrect(num1, num2, answer)}>입력</button> */}
       <button
-        // type="submit"
         onClick={() => {
           onClickCorrect();
-          // onClickSetNum();
         }}
       >
         입력
