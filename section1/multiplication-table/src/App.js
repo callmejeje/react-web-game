09d91fce9814f2cdca14ac336aefa822a39912cd
callmejeje <choije401@naver.com>
2022-08-04 20:54:22 +0900

    feat: 질문 밀리는 것 해결 완료

--- section1/multiplication-table/src/App.js
+++ section1/multiplication-table/src/App.js
@@ -2,23 +2,13 @@
 import "./App.css";
 import { useState } from "react";
 
-// let num1 = Math.ceil(Math.random() * 9);
-// let num2 = Math.ceil(Math.random() * 9);
-
-// function makeNum() {
-//   num1 = Math.ceil(Math.random() * 9);
-//   num2 = Math.ceil(Math.random() * 9);
-
-//   console.log("makeNum()");
-// }
-
-const Answer = ({ onChange }) => {
-  return (
-    <div className="answer">
-      <input type="number" onChange={onChange}></input>
-    </div>
-  );
-};
+// const Answer = ({ onChange }) => {
+//   return (
+//     <div className="answer">
+//       <input type="number" onChange={onChange}></input>
+//     </div>
+//   );
+// };
 
 function App() {
   const [answer, setAnswer] = useState(undefined);
@@ -29,63 +19,56 @@ function App() {
     Math.ceil(Math.random() * 9),
   ]);
 
-  const Question = () => {
-    return <div>{`${nums[0]} 곱하기 ${nums[1]}는?`}</div>;
-  };
+  // const Question = () => {
+  //   return <div>{`${nums[0]} 곱하기 ${nums[1]}는?`}</div>;
+  // };
 
-  function isCorrect(num1, num2, answer) {
-    /*
-    // console.log(
-    //   "num1 : " +
-    //     Number(num1) +
-    //     ", num2 : " +
-    //     Number(num2) +
-    //     ", answer : " +
-    //     Number(answer)
-    // );
-    // setCorrect(Number(num1) * Number(num2) === Number(answer));
-    // setAnswer(undefined);
-
-    // console.log("in isCorrect, correct : " + correct);
-
-    // if (correct) {
-    //   makeNum();
-    // }
-*/
-    return Number(num1) * Number(num2) === Number(answer);
-  }
+  // function isCorrect(num1, num2, answer) {
+  //   return Number(num1) * Number(num2) === Number(answer);
+  // }
 
   const onChangeAnswer = (e) => {
     setAnswer(e.target.value);
   };
 
   const onClickCorrect = () => {
-    setCorrect(isCorrect(nums[0], nums[1], answer));
+    // setCorrect(isCorrect(nums[0], nums[1], answer));
+    if (Number(nums[0]) * Number(nums[1]) === Number(answer)) {
+      setCorrect(true);
+      setNums([Math.ceil(Math.random() * 9), Math.ceil(Math.random() * 9)]);
+    } else {
+      setCorrect(false);
+    }
+    // onClickSetNum();
   };
 
-  const onClickSetNum = () => {
-    if (correct) {
-      setNums([Math.ceil(Math.random() * 9), Math.ceil(Math.random() * 9)]);
-    }
-  };
+  // const onClickSetNum = () => {
+  //   console.log("correct: " + correct);
+  //   if (correct === true) {
+  //     setNums([Math.ceil(Math.random() * 9), Math.ceil(Math.random() * 9)]);
+  //   } else {
+  //     let num0 = nums[0];
+  //     let num1 = nums[1];
+  //     setNums([num0, num1]);
+  //   }
+  // };
 
   return (
     <div className="App">
-      {/* <div>{`${num1} 곱하기 ${num2}는?`}</div> */}
-      <Question />
-      {/* <input type="number" value={answer} onChange={onChangeAnswer}></input> */}
-      <Answer onChange={onChangeAnswer} />
+      <div>{`${nums[0]} 곱하기 ${nums[1]}는?`}</div>
+      {/* <Question /> */}
+      <input type="number" onChange={onChangeAnswer}></input>
+      {/* <Answer onChange={onChangeAnswer} /> */}
       {/* <button onClick={() => isCorrect(num1, num2, answer)}>입력</button> */}
       <button
         // type="submit"
         onClick={() => {
           onClickCorrect();
-          onClickSetNum();
+          // onClickSetNum();
         }}
       >
         입력
       </button>
-      {/* {correct ? makeNum() : undefined} */}
       <div>{correct ? "성공!" : correct !== undefined ? "실패..." : ""}</div>
     </div>
   );
